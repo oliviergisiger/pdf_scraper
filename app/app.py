77669dd0b8a89022627181df58a453457311c1c8
oliviergisiger <olivier.gisiger@hslu.ch>
2025-10-06 11:09:41 +0200

    layout

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from scraper import WebPage
 from utils import make_zip
 
+TEMP_DIR = 'temp_pdfs'
 
 
 st.set_page_config(layout='centered')
@@ -14,12 +15,10 @@ col1, col2, col3 = st.columns([3, 2, 1])
 with col1:
     url = st.text_input('enter url')
 
-with col2:
-    directory = st.text_input('enter directory')
 
-with col3:
+with col2:
     st.markdown("<div style='height:1.7em'></div>", unsafe_allow_html=True)
-    go = st.button('go', use_container_width=True)
+    go = st.button('find pdfs', use_container_width=True)
 
 
 if go:
@@ -28,25 +27,25 @@ if go:
 
     page = WebPage(url)
     pdf_links = page.get_pdf_urls()
-    with st.spinner(f'Fetching {url} ...'):
-        if not pdf_links:
-            st.warning('no pdfs found.')
+    if not pdf_links:
+        st.warning('no pdfs found.')
 
-        else:
-            st.success(f'found {len(pdf_links)} pdfs.')
-            progress_bar = st.progress(0)
-            status = st.empty()
+    else:
+        st.success(f'found {len(pdf_links)} pdfs.')
+        progress_bar = st.progress(0)
+        status = st.empty()
 
-            total = len(pdf_links)
+        total = len(pdf_links)
 
-            for i, ref in enumerate(pdf_links, start=1):
-                page.download_pdf(ref, directory)
-                progress_bar.progress(i / total)
+        for i, ref in enumerate(pdf_links, start=1):
+            page.download_pdf(ref, TEMP_DIR)
+            progress_bar.progress(i / total)
 
-
-            zip_buffer = make_zip(directory)
+        with col3:
+            st.markdown("<div style='height:1.7em'></div>", unsafe_allow_html=True)
+            zip_buffer = make_zip(TEMP_DIR)
             st.download_button(
-                label='download .zip',
+                label='download',
                 data=zip_buffer,
                 file_name='pdfs.zip',
                 mime='application/zip'
